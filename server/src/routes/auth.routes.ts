d9306e866ef69b5e113502864554b66fe84ef202
mohmmad mazid <mazidmansury5500@gmail.com>
2026-09-01 13:20:55 +0530

    api updated

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -13,7 +13,7 @@ authRouter.post("/refresh", authRateLimiter, authController.refresh);
 authRouter.post("/logout", authController.logout);
 authRouter.get("/me", requireAuth, authController.me);
 //user delete route
-authRouter.delete("/delete-user", requireAuth, authController.deleteUser);
+authRouter.post("/delete-user", requireAuth, authController.deleteUser);
 
 
 authRouter.post(
